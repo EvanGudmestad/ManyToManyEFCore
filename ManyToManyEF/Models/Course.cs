f530b4c6f9770a979cac1c6e381480b42319cd70
EvanGudmestad <eagudmestad@ranken.edu>
2024-04-01 10:36:32 -0500

    Add project files.

---
+++ ManyToManyEF/Models/Course.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManyToManyEF.Models
+{
+    public class Course
+    {
+
+        public Course()
+        {
+            Students = new List<Student>(); //Initialize the list
+        }
+
+
+        //Primary key and identity
+        public int CourseId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        
+        //Skip navigation property
+        public List<Student> Students { get; set; }
+        
+    }
+}
---
+++ ManyToManyEF/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿namespace ManyToManyEF.Models
+{
+    public class Student
+    {
+
+        public Student()
+        {
+            Courses = new List<Course>(); //Initialize the list
+        }
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+
+        public string FinancialAidStatus { get; set; }
+
+        //Skip Navigation Property
+        public List<Course> Courses { get; set; }
+    }
+}
